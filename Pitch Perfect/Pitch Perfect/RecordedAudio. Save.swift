d703e2c580d48a801869e1ee27d9ae5912a936ad
RodrigoLondon <rodrigopeal@hotmail.com>
2015-04-05 22:51:24 +0100

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio. Save.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio. Save.swift
+//  Pitch Perfect
+//
+//  Created by user26512 on 4/3/15.
+//  Copyright (c) 2015 user26512. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
